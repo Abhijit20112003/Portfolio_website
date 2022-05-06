d3fcebf35cfb5086a0dc9d347179747754ce0083
Abhijit20112003 <roya51788@gmail.com>
2022-05-06 08:56:52 +0530

    initial commit

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -6,7 +6,7 @@ import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 const data = [
   { number: 5, text: 'Open Source Projects'},
   { number: 10, text: 'Hackathon', },
-  { number: 190, text: 'Github Followers', },
+  { number: 190, text: 'Twitter Followers', },
   { number: 20, text: 'Github Stars', }
 ];
 
